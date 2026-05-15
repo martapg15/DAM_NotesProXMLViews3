e34ac0b7839fe0725977f300517694e5f4189616
Marta Garcia <martapinto934@gmail.com>
2026-05-15 16:53:19 +0100

    edit existing notes (video tutorial)

--- app/src/main/java/com/notes/notesproxmlviews/NoteAdapter.kt
+++ app/src/main/java/com/notes/notesproxmlviews/NoteAdapter.kt
@@ -1,6 +1,7 @@
 package com.notes.notesproxmlviews
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,13 +10,22 @@ import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class NoteAdapter(options: FirestoreRecyclerOptions<Note>, context: Context) :
+class NoteAdapter(options: FirestoreRecyclerOptions<Note>, private val context: Context) :
     FirestoreRecyclerAdapter<Note, NoteAdapter.NoteViewHolder>(options) {
 
     override fun onBindViewHolder(holder: NoteViewHolder, position: Int, note: Note) {
         holder.titleTextView.text = note.title
         holder.contentTextView.text = note.content
         holder.timestampTextView.text = Utility.timestampToString(note.timestamp)
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(context, NoteDetailsActivity::class.java)
+            intent.putExtra("title", note.title)
+            intent.putExtra("content", note.content)
+            val docId = this.snapshots.getSnapshot(position).id
+            intent.putExtra("docId", docId)
+            context.startActivity(intent)
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
