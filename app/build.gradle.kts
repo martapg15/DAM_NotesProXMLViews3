881a65c8abedad037feb11dc6770a97461305d3a
Marta Garcia <martapinto934@gmail.com>
2026-05-15 16:36:20 +0100

    added dependencies for Cloud Firestore (FirebaseUI)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.firestore)
+    implementation(libs.firebase.ui.firestore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
